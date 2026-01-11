438233e0ab31b9d3a5d5520f5d4a97a43f087d68
Hamzah0499 <Hamzah-M@ulster.ac.uk>
2026-01-12 02:38:37 +0500

    Final Build

--- dist/utils/azureVision.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const analyzeImage: (imageBuffer: Buffer) => Promise<{
-    caption: any;
-    tags: any;
-    isSafe: boolean;
-    raw: any;
-} | null>;
-//# sourceMappingURL=azureVision.d.ts.map
